ad6dd8eb35e83494d5c8333cb9b2f24b20c229c4
Aleksandra Mitrović <aleksandra_94_lo@hotmail.com>
2019-12-03 09:19:02 +0100

    Add tabwidget and system dock

--- core/view/main_window.py
+++ core/view/main_window.py
@@ -2,6 +2,7 @@ from PySide2 import QtWidgets, QtCore, QtGui
 from ..model.document_model import DocumentModel
 from ..model.document import Document
 from ..model.page import Page
+from os.path import abspath
 
 
 # FIXME: Raspodeliti nadleznosti na druge view-ove.
@@ -20,7 +21,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.menubar = QtWidgets.QMenuBar(self)
         self.help_menu = QtWidgets.QMenu("Help")
         self.toolbar = QtWidgets.QToolBar(self)
-        self.central_widget = QtWidgets.QTableView(self)
+        self.central_widget = QtWidgets.QTabWidget(self)
         self.statusbar = QtWidgets.QStatusBar(self)
         self.project_dock = QtWidgets.QDockWidget(self)
 
@@ -50,18 +51,49 @@ class MainWindow(QtWidgets.QMainWindow):
     def _populate_main_window(self):
         # populisanje menija
         self._populate_menus()
-        # postavljanje modela u widget-e
-        self._set_models()
         # postavljanje widgeta na window
         self.setMenuBar(self.menubar)
         self.addToolBar(self.toolbar)
+        # populisanje tabova u centralnom widgetu
+        self._populate_tab_widget()
+        # postavljanje dock widgeta (mozemo ih imati proizvoljan broj)
+        self._populate_project_dock()
         self.setCentralWidget(self.central_widget)
         self.setStatusBar(self.statusbar)
-        # postavljanje dock widgeta (mozemo ih imati proizvoljan broj)
-        self.project_dock.setWidget(QtWidgets.QLabel("U izradi", self.project_dock))
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.project_dock)
         # uvezivanje akcija
         self._bind_actions()
+
+    def _populate_project_dock(self):
+        self.project_dock.setWidget(QtWidgets.QTreeView(self.project_dock))
+        model = QtWidgets.QFileSystemModel()
+        model.setRootPath(QtCore.QDir.currentPath())
+        self.project_dock.widget().setModel(model)
+
+    def _populate_tab_widget(self):
+        """
+            Populisati prilikom ucitavanja konteksta, kreirati sve tabove koji su bili otvoreni
+            sa widgetima. Podesiti modele za svaki ucitani widget.
+
+        """
+        # dodavanje ponasanja
+        self.central_widget.setTabsClosable(True)
+        # populisanje
+        # na osnovu liste modela se moze kreirati broj tabova
+        data_table = QtWidgets.QTableView(self.central_widget)
+        self.central_widget.addTab(data_table, QtGui.QIcon("resources/icons/icons8-grid-2-64.png"),
+            "Document data table1")
+        data_table = QtWidgets.QTableView(self.central_widget)
+        self.central_widget.addTab(data_table, QtGui.QIcon("resources/icons/icons8-grid-2-64.png"),
+            "Document data table2")
+        data_table = QtWidgets.QTableView(self.central_widget)
+        self.central_widget.addTab(data_table, QtGui.QIcon("resources/icons/icons8-grid-2-64.png"),
+            "Document data table3")
+
+        # postavljanje modela u widget-e iz tabova
+        self._set_models([self._dummy_document(), self._dummy_document(), self._dummy_document()])
+
+        self.central_widget.tabCloseRequested.connect(lambda i: self.central_widget.removeTab(i))
 
     def _populate_menus(self):
         """
@@ -70,8 +102,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.help_menu.addAction(self.menu_actions["about"])
         self.menubar.addMenu(self.help_menu)
 
-    def _set_models(self):
-        self.central_widget.setModel(self._dummy_document())
+    def _set_models(self, models=[]):
+        """
+            Populise modele u tabove redom.
+        """
+        print(self.central_widget.count())
+        for i in range(self.central_widget.count()):
+            widget = self.central_widget.widget(i)
+            widget.setModel(models[i])
 
     def _bind_actions(self):
         """
